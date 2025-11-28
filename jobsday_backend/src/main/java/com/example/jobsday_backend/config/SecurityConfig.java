3f324645145169a1caa561a3168c210f6716f110
datnehi <dndat1122@gmail.com>
2025-11-29 00:05:10 +0700

    deploy impl

--- jobsday_backend/src/main/java/com/example/jobsday_backend/config/SecurityConfig.java
+++ jobsday_backend/src/main/java/com/example/jobsday_backend/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://jobsday.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(Collections.singletonList(HttpHeaders.CONTENT_DISPOSITION));
